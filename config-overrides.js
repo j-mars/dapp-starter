8558cb0311a8b1837aae9a706e584f02d3489e6a
j-mars <jldcmartins@gmail.com>
2020-05-03 22:03:30 +0100

    final push

--- config-overrides.js
+++ config-overrides.js
@@ -1,8 +1,7 @@
 // eslint-disable-next-line
-const { override, addBabelPlugin, addWebpackAlias,  useEslintRc } = require('customize-cra');
+const { override, addBabelPlugin, addWebpackAlias } = require('customize-cra');
 
 module.exports = override(
-  useEslintRc(),
   addBabelPlugin('react-hot-loader/babel'),
   addWebpackAlias({
     'react-dom': '@hot-loader/react-dom',
--- src/router.jsx
+++ src/router.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import Loadable from 'react-loadable';
 import Loader from 'components/LayoutComponents/Loader';
 
@@ -7,7 +7,7 @@ const loadable = (loader) =>
   Loadable({
     loader,
     delay: false,
-    loading: () => <Loader />,
+    loading: () => <Loader spinning />,
   });
 
 const routes = [
