1cb8d1eeb2ade232dec72b18fa38c6dc70c944a2
j-mars <jldcmartins@gmail.com>
2020-05-03 22:55:55 +0100

    add antd

--- config-overrides.js
+++ config-overrides.js
@@ -1,9 +1,19 @@
 // eslint-disable-next-line
-const { override, addBabelPlugin, addWebpackAlias } = require('customize-cra');
+const { override, addBabelPlugin, addWebpackAlias, fixBabelImports, addLessLoader } = require('customize-cra');
 
 module.exports = override(
   addBabelPlugin('react-hot-loader/babel'),
   addWebpackAlias({
     'react-dom': '@hot-loader/react-dom',
+  }),
+  fixBabelImports('antd', {
+    libraryDirectory: 'es',
+    style: true,
+  }),
+  addLessLoader({
+    lessOptions: {
+      javascriptEnabled: true,
+      modifyVars: { '@primary-color': '#25b864' },
+    },
   }),
 );
--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { hot } from 'react-hot-loader/root';
 import logo from 'assets/icons/logo.svg';
 import Router from 'router';
+import { Button } from 'antd';
 import './assets/styles/App.css';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Hello</p>
+        <Button type="primary">Button</Button>
         <a
           className="App-link"
           href="https://reactjs.org"
